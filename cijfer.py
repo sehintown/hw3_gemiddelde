470db164994565fa1888debe8939fdfd8069b0f1
Salim <salim.elhamdioui@student.hu.nl>
2018-09-20 01:34:50 +0200

    cijferICOR veranderd

---
+++ cijfer.py
@@ -0,0 +1,14 @@
+cijferICOR = 7
+cijferPROG = 8
+cijferCSN = 7
+beloning = (cijferICOR + cijferPROG + cijferCSN) * 30
+print(beloning)
+
+listvalues = [7, 8, 7]
+listsum = sum(listvalues)
+listlength = len(listvalues)
+gemiddelde = listsum / listlength
+print(gemiddelde)
+
+a = 'Mijn cijfers van gemiddeld een 7.3333333333 leveren een beloning van € 660 op!'
+print(a)
